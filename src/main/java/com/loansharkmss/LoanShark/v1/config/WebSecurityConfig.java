cc55478ff178c825059537ccf77ebce43dad5be9
Darkrider123 <cosmin.ariton@gmail.com>
2022-12-11 18:06:29 +0200

    removed unnecessary code

--- src/main/java/com/loansharkmss/LoanShark/v1/config/WebSecurityConfig.java
+++ src/main/java/com/loansharkmss/LoanShark/v1/config/WebSecurityConfig.java
@@ -1,11 +1,9 @@
 package com.loansharkmss.LoanShark.v1.config;
 
 import com.loansharkmss.LoanShark.v1.service.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,14 +16,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     private final JwtRequestFilter jwtRequestFilter;
 
-    public WebSecurityConfig(UserService userService, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtRequestFilter jwtRequestFilter) {
-        this.userService = userService;
+    public WebSecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtRequestFilter jwtRequestFilter) {
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtRequestFilter = jwtRequestFilter;
     }
@@ -41,10 +36,6 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(EncryptionConfig.ENCRYPTION_STRENGTH);
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
-    }
 
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
