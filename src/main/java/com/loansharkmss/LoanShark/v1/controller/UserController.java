8b141ab4e7243f7845f16f9d8aa875f0027393a2
Darkrider123 <cosmin.ariton@gmail.com>
2023-01-13 17:12:19 +0200

    done

--- src/main/java/com/loansharkmss/LoanShark/v1/controller/UserController.java
+++ src/main/java/com/loansharkmss/LoanShark/v1/controller/UserController.java
@@ -1,17 +1,14 @@
 package com.loansharkmss.LoanShark.v1.controller;
 
 import com.loansharkmss.LoanShark.v1.config.RestControllerV1;
-import com.loansharkmss.LoanShark.v1.mappers.interfaces.UserMapper;
 import com.loansharkmss.LoanShark.v1.model.User;
 import com.loansharkmss.LoanShark.v1.service.interfaces.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Validated
 @RestControllerV1
 @RequestMapping("/user")
 public class UserController {
